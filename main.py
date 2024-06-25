e85dbeb8ef997a78bfc1602c75519d7c8ce1aec8
MotoMatt5040 <motomatt5040@gmail.com>
2024-06-25 14:37:48 -0500

    perf(main.py): inline file writing and error handling
    Adjusted script to write files as they are read so with error handling in case a file bugs. This will make it so the program doesnt halt if an error occurs, but rather the file is skipped.
    
    An error occured where a file under 1 second had no audible audio, the model inserted korean symbls however the ecoding was improper and the programming halted.

--- main.py
+++ main.py
@@ -1,42 +1,3 @@
-# Installing dependencies
-# This script is designed to run on Windows, but can be modified to run on other systems
-# Install chocolatey at https://chocolatey.org/install#individual
-# Make sure to use the individual installation
-# Here is the link provided for you at the time of writing this program 6/25/2024
-# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-# Copy the string text it provides and paste into an Administrative Powershell.
-# If you run into errors you may want to run the follow commands first
-# $chocolateyPath = "C:\ProgramData\chocolatey\bin"
-# $env:Path += ";$chocolateyPath"
-
-# Next install the FFMPEG package using the following command
-# choco install ffmpeg
-
-# Now, create a python virtual environment using
-# python -m venv venv
-# .\venv\Scripts\activate
-
-# PyTorch can now be installed, browse to https://pytorch.org/get-started/locally/
-# Here you will select your system options, only use "CUDA" if you have an NVIDIA
-# Here is the current cpu package
-# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-# Here is the current GPU package
-# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-
-# Run this command inside of your virtual environment terminal
-
-# Finally, run the following command
-# pip install openai-whisper
-
-# You how have all dependencies installed and can run the following script
-
-# Files can be transcribed through the command prompt or terminal
-# To do so, use the commend "whisper <filename>"
-# Multiple files can be done using "whisper <filename1> <filename2> <filename3>"
-# If there are spaces in your filename, use quotes around the file name
-# You can use --model to specify the model you want to use
-# The rest of this file will be a script created to run multiple files at one time
 import os
 import whisper
 
@@ -46,7 +7,7 @@ dir = os.path.join(cwd, "audio")
 audio_file_list = os.listdir(dir)
 
 error_report = []
-results = {}
+file_name = "Q4AOE_0000302814.wav"
 for file_name in audio_file_list:
     if not file_name.endswith(".wav"):
         continue
@@ -54,14 +15,16 @@ for file_name in audio_file_list:
     try:
         model = whisper.load_model("base")
         result = model.transcribe(file_path)
-        results[file_name] = result['text']
+        write_path = f"results/{file_name.replace('.wav', '')}.txt"
+        with open(write_path, "w", encoding='utf-8') as f:
+            try:
+                f.write(result['text'])
+            except:
+                print(f"{file_name}: Failed to write to file")
     except Exception as e:
+        print(f"{file_name}: Failed to load model\n    {e}")
         error_report.append(f"{file_name}: Failed to load model\n    {e}")
 
 if error_report:
     with open("error_report.txt", "w") as f:
         f.write("\n".join(error_report))
-
-for k, v in results.items():
-    with open(f"results/{k.replace('.wav', '')}.txt", "w") as f:
-        f.write(v)
