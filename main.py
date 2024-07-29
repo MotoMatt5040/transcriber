c1cfb192b35ef5cc3ac5bb6139fdff3ede30094e
matt@aero <motomatt5040@gmail.com>
2024-07-29 11:43:40 -0500

    update seconds

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from datetime import datetime
 #  Using this function will prevent from having to manually call other functions.
 t = Transcribe()
 while True:
+    sleep_time_in_minutes = 5
     current_hour = int(datetime.now().strftime("%H"))
     if not current_hour < 2 or not current_hour > 15:
         print("Sleeping for 30 minutes")
@@ -13,8 +14,8 @@ while True:
         continue
     t.transcribe()
     print('\n\n' + '-' * 50)
-    print('Sleeping for 30 minutes')
+    print(f'Sleeping for {sleep_time_in_minutes} minutes')
     print('-' * 50 + '\n\n')
-    time.sleep(60*30)
+    time.sleep(60 * sleep_time_in_minutes)
 
 
