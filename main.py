8ed9095b3e3f04f081830383c328f553b31e8588
matt@aero <motomatt5040@gmail.com>
2024-07-30 13:32:53 -0500

    formatting

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import torch
+from core.transcribe import Transcribe
+import time
+
+
 print("CUDA is available:", torch.cuda.is_available())
 print("CUDA devices available:", torch.cuda.device_count())
 print("Current CUDA device", torch.cuda.current_device())
@@ -6,28 +10,13 @@ print("Current CUDA device address", torch.cuda.device(0))
 print("Current CUDA device name:", torch.cuda.get_device_name(0))
 
 
-from core.transcribe import Transcribe
-import time
-# from datetime import datetime
-
-
-#  Please run self.transcribe to auto perform all necessary functions.
+#  Please run t.transcribe to auto perform all necessary functions.
 #  Using this function will prevent from having to manually call other functions.
 t = Transcribe()
 while True:
     sleep_time_in_minutes = 5
-    # current_time = datetime.now().strftime("%H:%M:%S")
-    # start_time = "16:00:00"
-    # end_time = "23:59:00"
-    # if start_time <= current_time or current_time <= end_time:
-    #     print(start_time, current_time, end_time)
-    #     print("Sleeping for 30 minutes")
-    #     time.sleep(60 * 30)
-    #     continue
     t.transcribe()
     print('\n\n' + '-' * 50)
     print(f'Sleeping for {sleep_time_in_minutes} minutes')
     print('-' * 50 + '\n\n')
     time.sleep(60 * sleep_time_in_minutes)
-
-
