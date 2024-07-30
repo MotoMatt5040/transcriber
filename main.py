8ef2e97742a07368d7757ad030cc707ddffb04f2
matt@aero <motomatt5040@gmail.com>
2024-07-30 12:03:28 -0500

    fix(main.py): remove schedule
    Removed the time schedule for running. It is currently not working properly and a solution will be put in place at a later time.
    
    BREAKING CHANGE: The program will need to be manually turned on when it is intended to run, the alternative is to leave it running and every 5 minutes the program will go to sleep if there is no job to be done.

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+import torch
+print("CUDA is available:", torch.cuda.is_available())
+print("CUDA devices available:", torch.cuda.device_count())
+print("Current CUDA device", torch.cuda.current_device())
+print("Current CUDA device address", torch.cuda.device(0))
+print("Current CUDA device name:", torch.cuda.get_device_name(0))
+
+
 from core.transcribe import Transcribe
 import time
-from datetime import datetime
+# from datetime import datetime
+
 
 #  Please run self.transcribe to auto perform all necessary functions.
 #  Using this function will prevent from having to manually call other functions.
 t = Transcribe()
 while True:
     sleep_time_in_minutes = 5
-    current_hour = int(datetime.now().strftime("%H"))
-    if not current_hour < 2 or not current_hour > 15:
-        print("Sleeping for 30 minutes")
-        time.sleep(60 * 30)
-        continue
+    # current_time = datetime.now().strftime("%H:%M:%S")
+    # start_time = "16:00:00"
+    # end_time = "23:59:00"
+    # if start_time <= current_time or current_time <= end_time:
+    #     print(start_time, current_time, end_time)
+    #     print("Sleeping for 30 minutes")
+    #     time.sleep(60 * 30)
+    #     continue
     t.transcribe()
     print('\n\n' + '-' * 50)
     print(f'Sleeping for {sleep_time_in_minutes} minutes')
