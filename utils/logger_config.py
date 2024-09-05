c8e95bf84d48c9b85fd1f00c951b75cdf1497646
matt@aero <motomatt5040@gmail.com>
2024-09-05 14:31:45 -0500

    fix(log): fix logging
    Fixed a bug where logging was rotating every minute. It has been updated to a weekly rotation. An environment variable was put in place to swap between dev and prod mode.

--- utils/logger_config.py
+++ utils/logger_config.py
@@ -2,10 +2,14 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from utils.logging_format import CustomFormatter
+import os
 
 # Set up the main logger
 logger = logging.getLogger('log')
-logger.setLevel(logging.DEBUG)
+if os.environ['environment'] == 'dev':
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
 
 # Console handler
 ch = logging.StreamHandler()
@@ -14,7 +18,7 @@ ch.setFormatter(CustomFormatter())
 logger.addHandler(ch)
 
 # File handler with monthly rotation
-fh = TimedRotatingFileHandler('logs/logs.log', when='M', interval=1, backupCount=3)
+fh = TimedRotatingFileHandler('logs/logs.log', when='W0', interval=1, backupCount=3)
 fh.setLevel(logging.WARNING)
 plain_formatter = logging.Formatter("%(asctime)s - %(name)s - %(filename)s - %(levelname)s - Line: %(lineno)d - %(message)s")
 fh.setFormatter(plain_formatter)
