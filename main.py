3ca3574dc64b75d759160edac5dc8fc6d4aff9ee
matt@aero <motomatt5040@gmail.com>
2025-01-27 12:21:46 -0600

    feat(main): add test
    Added test capabilities

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,20 +9,29 @@ import time
 import logging
 from logging.handlers import TimedRotatingFileHandler
 import traceback
+import sys
 
 from core.transcribe import Transcribe
 from utils.logger_config import logger
 
 
 parser = argparse.ArgumentParser(description="Transcribe audio files with specified settings.")
-parser.add_argument('--model', type=str, default='large', help='Model type to use for transcription (e.g., large, medium, small).')
+parser.add_argument('--model', type=str, default='medium', help='Model type to use for transcription (e.g., large, medium, small).')
 parser.add_argument('--sleep', type=int, default=5, help='Sleep duration in minutes between transcriptions.')
 
 args = parser.parse_args()
 
 
+if os.environ.get('environment') == 'test':
+    print('here')
+    from core.tests import Transcribe
+    t = Transcribe(model=args.model)
+    t.transcribe()
+    sys.exit()
+
 #  Please run t.transcribe to auto perform all necessary functions.
 #  Using this function will prevent from having to manually call other functions.
+
 t = Transcribe(model=args.model)
 while True:
     sleep_time_in_minutes = 5
